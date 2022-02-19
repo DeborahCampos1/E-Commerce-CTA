87e2f0c2fe3cf6536523ae870e3b37c734298f11
Deborah Campos <631care@gmail.com>
2022-02-18 19:35:11 -0500

    product database seeded

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,6 +1,20 @@
-DROP TABLE IF EXISTS test;
+DROP DATABASE IF EXISTS cta_dev;
+CREATE DATABASE cta_dev;
 
-CREATE TABLE test (
+\c cta_dev;
+
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT NOT NULL,
+    description TEXT,
+    image TEXT,
+    weight INTEGER,
+    price INTEGER,
+    rating INTEGER,
+    CHECK (rating >= 0 AND rating <= 5),
+    featured BOOLEAN,
+    in_stock BOOLEAN
 );
+
--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,8 +1,10 @@
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+\c cta_dev;
+
+INSERT INTO products (name, description, image, weight, price, rating, featured, in_stock) VALUES
+('1 oz Coin', 'American Gold Buffalo', 'https://online.kitco.com/products/1_oz_Gold_American_Buffalo_Coin_9999_2460/enu-1-oz-Gold-American-Buffalo-Coin-9999-2460-20000-2.jpg', 1, 2000,5, false, true),
+('1 oz Coin', 'American Gold Eagle', 'https://online.kitco.com/products/1_oz_Gold_American_Eagle_Coin_9167_3000/enu-1-oz-Gold-American-Eagle-Coin-9167-3000-40000-2.jpg', 1, 2005, 4, true, true),
+('1 oz Coin', 'Canadian Gold Maple Leaf', 'https://online.kitco.com/products/1_oz_Gold_Canadian_Maple_Leaf_Coin_9999_3110/enu-1-oz-Gold-Canadian-Maple-Leaf-Coin-9999-3110-40000-2.jpg', 1, 1995, 4, true, true),
+('1 oz Coin', 'Platinum American Eagle', 'https://online.kitco.com/products/2022-1-oz-Platinum-American-Eagle-Coin-9995-3005Y2022/enu-2022-1-oz-Platinum-American-Eagle-Coin-9995-3005Y2022-40000-2.jpg', 1, 1238, 5, false, true),
+('1 oz Coin', 'Platinum Canadian Maple Leaf', 'https://online.kitco.com/products/1_oz_Platinum_Canadian_Maple_Leaf_Coin_9995_3105/enu_1_oz_Platinum_Canadian_Maple_Leaf_Coin_9995_3105_40000_2.jpg', 1 , 1216, 4, true, true),
+('1 oz Bar', 'Platinum Valcambi Suisse', 'https://online.kitco.com/products/1-oz-Platinum-Valcambi-Suisse-Bar-9995-1017V/enu-1-oz-Platinum-Valcambi-Suisse-Bar-9995-1017V-40000-2.jpg', 1, 1197, 5, false, true),
+('25 oz Coins', 'Silver Maple Leaf Coins (25 pcs)', 'https://online.kitco.com/products/2022-1-oz-Silver-Maple-Leaf-Tube-25-coins-MintFirst-3104MFT2022/enu-2022-1-oz-Silver-Maple-Leaf-Tube-25-coins-MintFirst-3104MFT2022-50000-2.jpg', 25, 790, 5, true, true);
