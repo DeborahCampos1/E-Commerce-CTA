099d9974994593eb8e0f0dc469dd9884c654701b
Deborah Campos <631care@gmail.com>
2022-02-19 09:53:59 -0500

    img height change

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 function NavBar() {
     return (
       <nav id="nav" className="shimmer">
-        <h1><Link to="/products">See All Products</Link></h1>
+        <h2><Link to="/products">See All Products</Link></h2>
         <h1>Welcome to the Precious Metals Store</h1>
-        <h1><Link to="/products/new">Submit New Product</Link></h1>
+        <h2><Link to="/products/new">Submit New Product</Link></h2>
         
       </nav>
     );
