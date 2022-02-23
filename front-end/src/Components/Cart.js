d093b6d602a2c6fa2ee148273e1e45af45672c99
Deborah Campos <631care@gmail.com>
2022-02-23 12:16:46 -0500

    corrected NaN cart error

--- front-end/src/Components/Cart.js
+++ front-end/src/Components/Cart.js
@@ -4,12 +4,12 @@ import currencyFormatter from "./Utility"
 
 
 function Cart ({subtotal, itemname, itemcount, cartsub, itemlist}){
-    let totalprice = cartsub * 1.08;
+    let totalprice = subtotal * 1.08;
 
     return (
         <div id='cart'>
-            <h2>Subtotal: {currencyFormatter.format(cartsub)}</h2>
-            <h4>Tax: {currencyFormatter.format(cartsub * 0.08)}</h4>
+            <h2>Subtotal: {currencyFormatter.format(subtotal)}</h2>
+            <h4>Tax: {currencyFormatter.format(subtotal * 0.08)}</h4>
             <hr></hr>
             <h2>Total Price: {currencyFormatter.format(totalprice)}</h2>
             {/* <li>{itemlist}</li> */}
